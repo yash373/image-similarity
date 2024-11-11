efd309b9aeb104e7629fa6dd02a8aa0e794ecb99
Yashodhan Mandal <yashodhan.mandal.official@gmail.com>
2024-11-11 18:01:44 +0530

    Got upload images working

--- image_upload.py
+++ image_upload.py
@@ -1,1 +1,11 @@
+from imgurpython import ImgurClient
 
+def upload_image(path:str) -> None:
+    client = ImgurClient(client_id="cb30ede881b8f55",client_secret="7b67ef91de5555e1ce8929f13cac909b8e1dfaf6")
+    # Upload the image
+    uploaded_image = client.upload_from_path(path, anon=True)
+    # Return the URL of the uploaded image
+    return uploaded_image['link']
+
+# Test
+# upload_image("images.jpeg")
